b939aab0c07abac82afd8569c9bdb0765cf065d8
sjlee <sooji7876@gmail.com>
2020-12-09 16:41:01 +0900

    fixed untracked files

--- src/main/java/com/study/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/study/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.study.book.springboot.service.posts;
 
 import com.study.book.springboot.domain.posts.Posts;
 import com.study.book.springboot.domain.posts.PostsRepository;
+import com.study.book.springboot.web.dto.PostsListResponseDto;
 import com.study.book.springboot.web.dto.PostsResponseDto;
 import com.study.book.springboot.web.dto.PostsSaveRequestDto;
 import com.study.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor    //final이 선언된 모든 필드를 인자값으로 하는 생성자
 @Service
@@ -31,5 +35,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
---
+++ src/main/java/com/study/book/springboot/web/IndexController.java
@@ -0,0 +1,37 @@
+package com.study.book.springboot.web;
+
+import com.study.book.springboot.service.posts.PostsService;
+import com.study.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        //src/main/resource/templates
+        //확장자는 .mustache
+        model.addAttribute("posts", postsService.findAllDesc());
+        return  "index";//머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
